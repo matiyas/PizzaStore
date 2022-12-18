a415eb3dea1c10e96f62b8c901212d9b80b643e2
matiyas <tougepl@gmail.com>
2022-12-18 15:07:21 +0100

    Add in memory db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.Db;
+using PizzaStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c => {
   var info = new OpenApiInfo {
     Title = "Todo API",
@@ -20,10 +22,12 @@ app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1"));
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/pizzas", () => PizzaDb.GetPizzas());
-app.MapGet("/pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDb.RemovePizza(id));
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) => {
+  await db.Pizzas.AddAsync(pizza);
+  await db.SaveChangesAsync();
+
+  return Results.Created($"/pizzas/{pizza.Id}", pizza);
+});
 
 app.Run();
